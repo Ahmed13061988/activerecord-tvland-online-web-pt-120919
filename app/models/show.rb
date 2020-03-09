09e27610c93dd7c4055fae6ee9b0970772414dd2
Ahmed Hussein <wolktheline@yahoo.com>
2020-03-09 23:07:53 +0000

    Automatically backed up by Learn

--- app/models/show.rb
+++ app/models/show.rb
@@ -11,8 +11,7 @@ class Show < ActiveRecord::Base
   def actors_list
    array=[]
    array_full_name =self.actors.map {|actor| actor.full_name}
-   array_of_shows = self.shows.map {|show| show.name}
-   show = array_full_name.concat array_of_shows
+   show = array_full_name
     array << show.join(" - ")
     array
    
